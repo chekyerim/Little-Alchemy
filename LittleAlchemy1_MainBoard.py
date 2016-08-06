ff70661aa7eaaea47f877590d15aeac2fedeeb71
Jinhyeok1489 <songjinhuk@dgist.ac.kr>
2016-08-06 18:53:49 +0900

    Some functions were added.
    1. I don't know what tuple is.
    2. But I added money font, and healthbar
    3. But it seems that my work doesn't work well because 1.
    Later
    1. we need to show how many health_gauge was left
    2. The most important Function for this game :  make the target to
    follow the road
    1- Differentiate color (The easiest if it can)
    2- Make the target move for every stage (It would be very hard mode for
    us)
    3- To make our tower attack if target is in certain circular range.

--- LittleAlchemy1_MainBoard.py
+++ LittleAlchemy1_MainBoard.py
@@ -1,6 +1,7 @@
 #1. Let's Import Library!
 import pygame,os, sys
 from pygame.locals import *
+import random
 #1-1. Make it easier
 
 
@@ -13,14 +14,23 @@ pos_steam=[250,700]
 pos_wood=[400,700]
 pos_lib=[pos_fire,pos_steam,pos_wood]
 keys=[False,False,False,False]
+monstimer=100 #monster 타이머 : 얘가 0이 되면 리젠
+monstimer1=0 #monstimer가 0이 되도록 조절한다.
+monspos=[[640,100]]
+healthvalue=190
 
 #3. Load images
 fire=pygame.image.load("resources/images/fire.jpg")
 steam=pygame.image.load("resources/images/steam_gas.jpg")
 wood=pygame.image.load("resources/images/Wood.jpg")
+monsterimg1=pygame.image.load("resources/images/dude.png") #monsterimage를 추가한다.
+monsterimg=monsterimg1
+healthbar=pygame.image.load("resources/images/healthbar.png")
+health=pygame.image.load("resources/images/health.png")
 
 #4. keep looping through
 while 1:
+    monstimer-=1
     #5. clear the screen before drawing it again
     screen.fill(0)
 
@@ -28,7 +38,36 @@ while 1:
     screen.blit(fire, pos_fire)
     screen.blit(steam, pos_steam)
     screen.blit(wood, pos_wood)
-    #6-1. Method for moving elements
+    #6-1. Show me the monster (timer가 0이 될때마다 monster가 나오게 한다.)
+    if monstimer==0:
+        monster.append([640, random.randint(50,430)]) #[640, 50~430 랜덤이라는 좌표를 추가한다.
+        monstimer=100-(monstimer1*2)
+        if monstimer1>=35:
+            monstimer1=35
+        else:
+            monstimer1+=5
+    index=0
+    for monster in monspos:
+        if monster[0]<=0:
+            monspos.pop(index)
+        monspos[0][0]-=3
+    index+=1
+    for monster in monspos:
+        screen.blit(monsterimg1, monster) ###### 아직 에러가 있음.. 몬스터가 특정 위치로 가면 사라짐;; 얘도 tuple로 인한 문제로 보임.
+
+    #6-2. Show me the money (monster가 체력이 달아 없어질 때마다 돈이 더해진다.)
+        font = pygame.font.Font(None, 24)
+        money=0
+        # if num_mons-=1:
+        #money+=20
+        show_text=font.render(str(money)+":"+str('$').zfill(2), True, (255,255,0))
+        textRect= show_text.get_rect()
+        textRect.topright=[1000,5]
+        screen.blit(show_text,textRect)
+    #6-3. Draw health bar
+    screen.blit(healthbar,(monspos[0][0], monspos[0][1])) #피통은 구현함..
+    #for health1 in range(0,150):
+       # screen.blit(health, (health1*8.8)) #얘는 에러가 난다. 원래 blit의 두번째 칸에는 좌표나 tuple, 근데 tuple이 뭔지 모름
 
 
     #7. update the screen
